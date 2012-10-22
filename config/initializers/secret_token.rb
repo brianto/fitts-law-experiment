bd48a691bd4538135f0917d7458751064e27fbed
Brian To <bxt5647@rit.edu>
2012-10-22 00:40:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FittsLaw::Application.config.secret_token = '206c7e34193e8126eb60daf90b0bd250e42e7d45605aff7e4b29bd8d3471f723031db5160737efbcb0fbb333c5f223e9dcecc360885f533d78daa0c4d572ee2f'
